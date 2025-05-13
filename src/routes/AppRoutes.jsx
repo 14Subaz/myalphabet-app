44a9787148525bcdd7f3b533d05f2fe84a27bb8d
Subash_S <subash.sankaralingam@alphabet.school>
2025-05-13 16:37:45 +0530

    Initial commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* Define module routes here */}
+    </Routes>
+  );
+}
+
+export default AppRoutes;
